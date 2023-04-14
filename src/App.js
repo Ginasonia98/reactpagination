fb37e9b68043dcc03e0c168d3cd3b72042b43de9
Ginachiko1998 <soniagina669@gmail.com>
2023-04-14 22:14:32 +0700

    make pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ReactPaginate from "react-paginate";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [pageCount, setpageCount] = useState(0);
+
+  let limit = 10;
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      const res = await fetch(
+        `http://localhost:3004/photos?_page=1&_limit=${limit}`
+      );
+      const data = await res.json();
+      const total = res.headers.get("x-total-count");
+      setpageCount(Math.ceil(total / limit));
+      // console.log(Math.ceil(total/12));
+      setItems(data);
+    };
+
+    getPhotos();
+  }, [limit]);
+
+  const fetchPhotos = async (currentPage) => {
+    const res = await fetch(
+      `http://localhost:3004/photos?_page=${currentPage}&_limit=${limit}`
+    );
+    const data = await res.json();
+    return data;
+  };
+
+  const handlePageClick = async (data) => {
+    console.log(data.selected);
+
+    let currentPage = data.selected + 1;
+
+    const photosFormServer = await fetchPhotos(currentPage);
+    setItems(photosFormServer);
+    // scroll to the top
+    //window.scrollTo(0, 0)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row m-2">
+        {items.map((item) => {
+          return (
+            <div key={item.id} className="col-sm-6 col-md-4 v my-2">
+              <div className="card shadow-sm w-100" style={{ minHeight: 225 }}>
+                <div className="card-body">
+                  <h4 className="card-title text-center h2">{item.id} </h4>
+                  <h4 className="card-title text-center h2">{item.title} </h4>
+                  <h6 className="card-subtitle mb-2 text-muted text-center">
+                    {item.url}
+                  </h6>
+                  <p className="card-text">{item.thumbnailUrl}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        breakClassName={"page-item"}
+        breakLinkClassName={"page-link"}
+        activeClassName={"active"}
+      />
     </div>
   );
 }
